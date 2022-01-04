17fa51758b50f5703e4038b09b2b8c54f5893517
Phillip Langland <Phillip.Langland@chiprewards.com>
2022-01-04 17:18:01 -0600

    init commit

---
+++ lib/artoo_nerves.ex
@@ -0,0 +1,18 @@
+defmodule ArtooNerves do
+  @moduledoc """
+  Documentation for ArtooNerves.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ArtooNerves.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/artoo_nerves_test.exs
@@ -0,0 +1,8 @@
+defmodule ArtooNervesTest do
+  use ExUnit.Case
+  doctest ArtooNerves
+
+  test "greets the world" do
+    assert ArtooNerves.hello() == :world
+  end
+end
